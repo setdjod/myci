19293494aabbb1140ee55a365203bb596badc83a
setdjod <setdjod@gmail.com>
2019-05-08 07:43:50 +0700

    Frist Commit

---
+++ app/config/autoload.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['libraries']	= [];
+$autoload['packages']		= [];
+$autoload['language']		= [];
+$autoload['drivers']		= [];
+$autoload['helper']			= [];
+$autoload['config']			= [];
+$autoload['model']			= [];
